b58da04ac01c00f119ebe355ca7187ba6d351b89
Keith Burdis <keith@burdis.org>
2013-01-24 20:51:14 +0000

    Don't flush the id cache on every Puppet run.

--- manifests/client/service.pp
+++ manifests/client/service.pp
@@ -17,22 +17,6 @@ class autohome::client::service {
           enable     => true,
           require    => Class['autohome::client::config'],
         }
-	# Flush the uid and gid cache to avoid 'nobody:nobody' file and
-	# directory ownership issues
-        case $operatingsystemrelease {
-          '6.3': {
-            exec { 'clear-idmapd-cache':
-              command => '/usr/sbin/nfsidmap -c',
-              require => Service['rpcidmapd'],
-            }
-          }
-          default: {
-            exec { 'restart-rpcidmapd':
-              command => '/sbin/service rpcidmapd restart',
-              require => Service['rpcidmapd'],
-            }
-          }
-        }
       }
       default: {
         warning("$operatingsystem not currently supported")
